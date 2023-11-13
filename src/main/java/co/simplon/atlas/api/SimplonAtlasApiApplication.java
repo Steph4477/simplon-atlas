3aea9001e6265f534c7fc37a98da9f349ece0aee
Stéphane Morel <110528810+Steph4477@users.noreply.github.com>
2023-11-13 16:31:08 +0100

    test back

---
+++ src/main/java/co/simplon/atlas/api/SimplonAtlasApiApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.atlas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplonAtlasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplonAtlasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/simplon/atlas/api/SimplonAtlasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.atlas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplonAtlasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
