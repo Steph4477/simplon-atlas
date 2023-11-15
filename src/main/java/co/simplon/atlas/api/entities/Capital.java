2b4837063f0f79a227b3ccb8b41240fcdf3b06ab
Stéphane Morel <110528810+Steph4477@users.noreply.github.com>
2023-11-15 16:24:52 +0100

    capital

---
+++ src/main/java/co/simplon/atlas/api/entities/Capital.java
@@ -0,0 +1,31 @@
+package co.simplon.atlas.api.entities;
+
+import java.util.UUID;
+
+class Capital{
+	private UUID id;
+	private String name;
+	private double area;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+
+	
+
+	
+}
